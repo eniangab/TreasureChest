a1a1ec8fd1b775fb067fead1711c0b6f7a4fb4df
Asuquo Eniang <asuquo.eniang@tc.gc.ca>
2025-08-13 16:03:11 -0400

    Fixed bible links

--- ecotb.js
+++ ecotb.js
@@ -18,8 +18,8 @@ function  openBibleRef(bookVerse){
 }
 
 let settings = {
-    bibleUrlPrefix: 'https://www.biblegateway.com/passage/?search=', //gateway
-    bibleUrlPostfix: '&version=NIV1984',
+    bibleUrlPrefix: 'https://bibleportal.com/passage/?search=', //gateway
+    bibleUrlPostfix: '&version=ESV1984',
     biblePortalUrlPrefix: 'https://bibleportal.com/passage?search=',
-    biblePortalUrlPostfix: '&version=NIV1984'
+    biblePortalUrlPostfix: '&version=ESV1984'
 };
